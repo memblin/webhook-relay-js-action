7fb369ed6be5239657fed07c93364ba110c44511
Chris Row <1418370+memblin@users.noreply.github.com>
2024-09-13 16:14:26 -0600

    fix: adjustments for SSL target servers

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const { v4: uuidv4 } = require('uuid');
 // Default Environment Variables
 const GITHUB_EVENT_PATH = process.env.GITHUB_EVENT_PATH || null;
 const GITHUB_EVENT_NAME = process.env.GITHUB_EVENT_NAME || null;
-const RUNNER_NAME = process.env.RUNNER_NAME || null;
 
 function getSignatures(payload, secret) {
   // Generate X-Hub-Signature
@@ -30,11 +29,15 @@ async function main() {
     const webhookEndpoint = core.getInput('WEBHOOK_ENDPOINT');
     const webhookSecret = core.getInput('WEBHOOK_SECRET');
 
+    // Create a URL object from our webhookEndpoint variable to allow
+    // us to use URL.hostname in our host header to make SSL happy
+    const webhookUrl = new URL(webhookEndpoint);
+
     const { sha1Signature, sha256Signature } = getSignatures(payloadString, webhookSecret);
 
     // Set the headers
     const headers = {
-      'Host': RUNNER_NAME,
+      'Host': webhookUrl.hostname,
       'User-Agent': 'WebHook-Relay-Action',
       'Content-Length': `${payloadString.length}`,
       'Accept': '*/*',
